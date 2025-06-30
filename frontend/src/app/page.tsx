aa033ea8f34f0c91363aa233be30430fa632c83a
Sherwin Rodriguez <sherwin@example.com>
2025-06-30 16:34:43 +0530

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-4xl font-bold text-black">Tailwind v3 is Working 🚀</h1>
+    </div>
+  );
+}
